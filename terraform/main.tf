33e6d9351e1100b8f111656a078e3ee72b068838
shanrijo <smrijo235@gmail.com>
2023-11-18 22:03:14 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,6 @@ variable "vpc_id" {
   description = "ID of the VPC where the security group will be created"
 }
 
-variable "security_group_name" {
-  description = "Name of the security group"
-  default     = "my-security-group"
-}
 
 variable "security_group_exists" {
   description = "Indicates whether the security group already exists"

5437951b9580dbd58259382445764a23980c50a9
shanrijo <smrijo235@gmail.com>
2023-11-18 21:54:16 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,3 @@ resource "aws_security_group" "new" {
   // Add more ingress rules as needed
 }
 
-output "security_group_id" {
-  value = aws_security_group.new.arn
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "instance_public_ip" {
 output "instance_public_dns" {
   value = aws_instance.aws_ubuntu.public_dns
 }
+
+output "security_group_id" {
+  value = aws_security_group.new[count.index].arn
+}
